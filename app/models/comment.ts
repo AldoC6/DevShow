d4767fe7128b390f813fc1ebcf9a839f9d99fba3
Aldo Cavazos <aldocaro158@gmail.com>
2026-06-12 23:55:07 -0600

    relaciones en modelos para comments y posts

--- app/models/comment.ts
+++ app/models/comment.ts
@@ -1,4 +1,15 @@
 import { CommentSchema } from '#database/schema'
+import { belongsTo } from '@adonisjs/lucid/orm'
+import User from './user.ts'
+import { type BelongsTo } from '@adonisjs/lucid/types/relations'
+import Post from './post.ts'
 
 export default class Comment extends CommentSchema {
+
+    @belongsTo(() => Post)
+    declare post: BelongsTo<typeof Post>
+
+    @belongsTo(() => User)
+    declare user: BelongsTo<typeof User>
+
 }
--- app/models/post.ts
+++ app/models/post.ts
@@ -1,4 +1,16 @@
 import { PostSchema } from '#database/schema'
+import { hasMany, belongsTo } from '@adonisjs/lucid/orm'
+import type { HasMany, BelongsTo } from '@adonisjs/lucid/types/relations'
+import Comment from '#models/comment'
+import User from '#models/user'
+
 
 export default class Post extends PostSchema {
+
+    @hasMany(() => Comment)
+    declare comments: HasMany<typeof Comment>
+
+    @belongsTo(() => User)
+    declare user: BelongsTo<typeof User>
+
 }
